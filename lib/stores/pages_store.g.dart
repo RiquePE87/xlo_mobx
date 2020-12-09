c76feb2661c8dacaf9df29f2ab7a56f020bc61bc
RiquePE87 <rickpe81@gmail.com>
2020-12-09 09:05:10 -0300

    0712

---
+++ lib/stores/pages_store.g.dart
@@ -0,0 +1,46 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'pages_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$PagesStore on _PagesStore, Store {
+  final _$pageAtom = Atom(name: '_PagesStore.page');
+
+  @override
+  int get page {
+    _$pageAtom.reportRead();
+    return super.page;
+  }
+
+  @override
+  set page(int value) {
+    _$pageAtom.reportWrite(value, super.page, () {
+      super.page = value;
+    });
+  }
+
+  final _$_PagesStoreActionController = ActionController(name: '_PagesStore');
+
+  @override
+  void setPage(int value) {
+    final _$actionInfo =
+        _$_PagesStoreActionController.startAction(name: '_PagesStore.setPage');
+    try {
+      return super.setPage(value);
+    } finally {
+      _$_PagesStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+page: ${page}
+    ''';
+  }
+}
